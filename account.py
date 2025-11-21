74b8cbb0192889457fc5a257c3bd5821ebbb2dc4
Brian kimanzi <migelbrian3@gmail.com>
2025-11-21 22:25:46 +0300

    new code update

---
+++ account.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def auth():
+    st.title('Welcome to :grey[WakeApp]')
+
+    choose = st.selectbox('Login/SignUp', ['Login', 'Sign Up'])
+    if choose == 'Login':
+        email = st.text_input('Email address')
+        password = st.text_input('Password', type='password')
